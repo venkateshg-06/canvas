054a89fe0e31cb37ae9887c37be0aa2cca9fb5da
unknown <venkateshgolla134@gmail.com>
2024-03-20 22:17:47 +0530

    hello world1

--- script.js
+++ script.js
@@ -1,52 +1,53 @@
-let canvas = document.querySelector('#draw')
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight 
-const ctx = canvas.getContext('2d')
-ctx.strokeStyle = "red";
-ctx.lineJoin = "round";
-ctx.lineCap = "round";
-ctx.lineWidth = 10;
-let hue  = 0;
-let width = 50; 
-let istrue = true 
+const canvas = document.querySelector('#draw');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.strokeStyle = '#BADA55';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 100;
+// ctx.globalCompositeOperation = 'multiply';
 
-let isDrawing = false ;
-let lastX = 0 ;
-let lastY = 0 ; 
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+let direction = true;
 
-function draw(e){
-    if (!isDrawing) return 
-    ctx.beginPath();
-    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`
-    ctx.moveTo(lastX, lastY)    
-    ctx.lineTo(e.offsetX, e.offsetY)
-    ctx.stroke();
-    [lastX,lastY] = [e.offsetX,e.offsetY]
-   ctx.lineWidth = width
-    if (width>= 50 || width <=1){
-      istrue = !istrue
-    }
-    if(istrue){
-        width++
-    }
-    else {
-        width--
-    }
+function draw(e) {
+  if (!isDrawing) return; // stop the fn from running when they are not moused down
+  console.log(e);
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+  ctx.beginPath();
+  // start from
+  ctx.moveTo(lastX, lastY);
+  // go to
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  [lastX, lastY] = [e.offsetX, e.offsetY];
 
+  hue++;
+  if (hue >= 360) {
+    hue = 0;
+  }
+  if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+    direction = !direction;
+  }
 
-    if (hue >= 360){
-        hue  = 0 ;
-    }
-    hue++
-    
+  if(direction) {
+    ctx.lineWidth++;
+  } else {
+    ctx.lineWidth--;
+  }
+
 }
 
-canvas.addEventListener("mousedown", (e) => {
-    isDrawing = true 
-    [lastX,lastY] = [e.offsetX,e.offsetY]
- } );
+canvas.addEventListener('mousedown', (e) => {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
 
-canvas.addEventListener("mousemove", draw)
 
-canvas.addEventListener("mouseup",() => isDrawing = false)
-canvas.addEventListener("mouseout",() => isDrawing = false)
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
